6907351cec661761c71a552221fcabce60550a36
neozaru <neozaru@mailoo.org>
2021-04-21 09:11:05 +0200

    dev config to other account

--- config.dev.js
+++ config.dev.js
@@ -1,7 +1,7 @@
 module.exports = {
   API_URL: 'https://api.deversifi.dev',
-  PRIVATE_KEY: 'f083c6056578a6908aa41619c33491fe5289be5763f17c502e6ddb9bc41ec403',
-  STARK_KEY: 'ed86ee300e6d713fa16cfa052c85fd68d9509cf68ccc63bb889169b34ea2a8bc',
+  PRIVATE_KEY: '1eec4de4ceaf0fe48a05c01764df6780237144ee0037cbd158c6a1760660b96c',
+  STARK_KEY: 'a49efa97f2931bf0b839332e0c7cd60db43572e908f9c228b032ffca52ebe0db',
   BFX_WS: 'wss://api-pub.bitfinex.com/ws/2',
   PAIR: 'CUSDT:USDT',
   ALCHEMY_URL: 'wss://eth-ropsten.ws.alchemyapi.io/v2/I8GLrW7drrTiLi0hWtMR_Tih4KebU3dy'
