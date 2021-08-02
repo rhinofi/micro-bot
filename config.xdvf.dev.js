c5419c14fbb10010fc3d7a57b5973055c50ac649
neozaru <neozaru@mailoo.org>
2021-08-02 15:33:35 +0200

    xDVF market making + refactoring

---
+++ config.xdvf.dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  API_URL: 'https://api.deversifi.dev',
+  PRIVATE_KEY: '7c8e4c8d13bd49394ea6cf638684cd61ec8d94ae271e2277249fb704dd2c1de4',
+  STARK_KEY: '3ed65609618afb5d23f76973c3454656637a82a211eaad5c01e184fa02de0e58',
+  BFX_WS: 'wss://api-pub.bitfinex.com/ws/2',
+  PAIR: 'DVF:xDVF',
+  ALCHEMY_URL: 'wss://eth-ropsten.ws.alchemyapi.io/v2/I8GLrW7drrTiLi0hWtMR_Tih4KebU3dy'
+}
--- cusdt.js
+++ cusdt.js
@@ -1,119 +1,10 @@
-const DVF = require('./dvf')
-const _ = require('lodash')
-const { splitSymbol, prepareAmount, preparePrice } = require('dvf-utils')
-
-const configFileName = process.env.CONFIG || './config'
+const splitSymbol = require('dvf-utils/lib/splitSymbol');
 const request = require('request-promise')
 
-const { PAIR, STARK_KEY, ALCHEMY_URL } = require(configFileName)
+const { startMarketMaker } = require('./marketMaker');
 
-let dvf
-
-let lastMidPrice
-
-let tokenQuote
-let tokenBase
-
-onStartUp()
-
-let pair, routeBuy, routeSell, buySide, sellSide, midPrice
-async function marketMake () {
-  periodicReplace()
-  setInterval(periodicReplace, 600000)
-}
-
-async function periodicReplace() {
-  try {
-    midPrice = await getCoingeckoPrice()
-    console.log(midPrice)
-  } catch (e) {
-    console.error('Coingecko price error, using previous price', e)
-  }
-  const haveOpenOrders = await checkIfOpenOrders()
-  if (midPrice !== lastMidPrice || !haveOpenOrders) {
-    lastMidPrice = midPrice
-    replaceOrders()
-  }
-}
-
-async function onStartUp () {
-  dvf = await DVF()
-  await syncBalances()
-  console.log('Starting balances: ', balanceA, balanceB)
-  marketMake()
-}
-
-// Trading Functions
-
-let balanceA
-let balanceB
-
-async function cancelOpenOrders () {
-  const orders = await dvf.getOrders()
-  orders.forEach(o => {
-    if (o.symbol != PAIR) return
-    dvf.cancelOrder(o._id)
-  })
-}
-
-async function checkIfOpenOrders () {
-  const orders = await dvf.getOrders()
-  return orders.length > 0
-}
-
-async function syncBalances () {
-  const apiReturnedBalances = await dvf.getBalance()
-  const balances = _.chain(apiReturnedBalances)
-    .keyBy('token')
-    .mapValues('available')
-    .value()
-  const [quote, base] = splitSymbol(PAIR)
-  console.log('balances', balances)
-  balanceA = dvf.token.fromQuantizedAmount(quote, balances[quote])
-  balanceB = dvf.token.fromQuantizedAmount(base, balances[base])
-  balanceA = balanceA === 'NaN' ? 0 : balanceA
-  balanceB = balanceB === 'NaN' ? 0 : balanceB
-}
-
-async function replaceOrders () {
-  cancelOpenOrders()
-  await syncBalances()
-  const balanceToSell = Math.min(0.9 * balanceA, 75000 / lastMidPrice)
-  placeOrder(-1 * balanceToSell)
-  const balanceToBuy = Math.min(0.9 * balanceB, 100000) / lastMidPrice
-  placeOrder(balanceToBuy)
-}
-
-async function placeOrder (amount) {
-  amount = 100 * Math.trunc(prepareAmount(amount, 0) / 100)
-  if (amount == 0) return
-
-  const [quote, base] = splitSymbol(PAIR)
-  let price
-  if (amount > 0) {
-    price = preparePrice(lastMidPrice * 0.999)
-    console.log('Place buy at:', price)
-  } else {
-    price = preparePrice(lastMidPrice * 1.001)
-    console.log('Place sell at:', price)
-  }
-  if (!price) return
-
-  console.log(amount, price)
-
-  try {
-    await dvf.submitOrder({
-      symbol: PAIR,
-      amount,
-      price,
-      starkPrivateKey: STARK_KEY
-    })
-  } catch (e) {
-    // console.log('error e', e)
-    const error = (e.error && e.error.details && e.error.details.error) || {}
-    console.warn(`Trade not completed: ${error.error}`)
-  }
-}
+// Starts maket-making with 1:1
+startMarketMaker(getCoingeckoPrice)
 
 // Forces known mainnet addresses
 // Useful when running on dev and stg
@@ -122,8 +13,8 @@ const envContractAddressesOverride = {
 }
 
 let config, quote, base
-async function getCoingeckoPrice () {
-  [quote, base] = splitSymbol(PAIR)
+async function getCoingeckoPrice (dvf, pair) {
+  [quote, base] = splitSymbol(pair)
   let tokenContractAddress = envContractAddressesOverride[quote]
   
   if (!tokenContractAddress) {
@@ -141,4 +32,3 @@ async function getCoingeckoPrice () {
 
   return parseFloat(apiResponse.cToken[0].exchange_rate.value).toPrecision(4)
 }
-
